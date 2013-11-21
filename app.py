efe4693a64727526e27a9e0cfe60e201a3d1e684
Nick HS <nickhs@umich.edu>
2013-11-21 02:08:18 -0500

    Add in raven

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.mail import Mail
 from flask.ext.debugtoolbar import DebugToolbarExtension
 from flask.ext.login import LoginManager
+from raven.contrib.flask import Sentry
 
 app = Flask(__name__)
 
@@ -34,6 +35,9 @@ app.config['DEBUG'] = bool(os.environ.get('DEBUG', False))
 
 if app.config['SQLALCHEMY_DATABASE_URI']:
     db = SQLAlchemy(app)
+
+if app.config.get('SENTRY_DSN', None):
+    sentry = Sentry(app)
 
 config = app.config
 
